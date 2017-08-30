f50baac768cc8b3e27c6a911fc493467ff5d2368
raghvendranaidu <raghvendranaidu.naidu@gmail.com>
2017-08-30 12:45:27 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAUNO8l4WiYBALwXnj92EPZBaZBNaOg5Ye8hqJP2eIRhar74olcOraDN0J1Lqixic37SrNBChG5MywF0xbOyewNgkWuhqjDhZBKJGyXKM127n6YcRZAeLNL4KWX1Y4O48VQWGZCp7qCrxwlRtdi12mpZCM3O6ZBp6DcWNFYruq7wPNRFZA1TkRz7',
+    FB_VERIFY_TOKEN: 'my_testbot_token',
+    API_AI_CLIENT_ACCESS_TOKEN: '8ca89448ecd048bb82fbbf62e3f9de0f',
+    FB_APP_SECRET: 'ce6f59f0e98b1f037beaae855f56d3b6',
+    SERVER_URL: "https://raghvendra-testbot.herokuapp.com/",
+    SENDGRID_API_KEY:'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',
+    EMAIL_FROM:"raghvendranaidu.naidu@gmail.com",
+    EMAIL_TO:"raghvendranaidu.naidu@gmail.com",
+    PG_CONFIG: {
+	user: 'prasvxvvxgemxk',
+	database: 'dchc5do5hsjbv3',
+	password: '345bcc1e574723a287d6f4b05717ffab2890f592fc0ed60281a764cd2115ace5',
+	host: 'ec2-54-163-236-33.compute-1.amazonaws.com',
+	port: 5432,
+	max: 10,
+	idleTimeoutMillis: 30000,
+}
+
+};
+
+
