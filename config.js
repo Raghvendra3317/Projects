3c4301632159e57a7ede15be5a6ef1c3cb3cad0c
raghvendranaidu <raghvendranaidu.naidu@gmail.com>
2017-08-30 13:15:25 +0530

    updating

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAUNO8l4WiYBALwXnj92EPZBaZBNaOg5Ye8hqJP2eIRhar74olcOraDN0J1Lqixic37SrNBChG5MywF0xbOyewNgkWuhqjDhZBKJGyXKM127n6YcRZAeLNL4KWX1Y4O48VQWGZCp7qCrxwlRtdi12mpZCM3O6ZBp6DcWNFYruq7wPNRFZA1TkRz7',
-    FB_VERIFY_TOKEN: 'my_testbot_token',
-    API_AI_CLIENT_ACCESS_TOKEN: '8ca89448ecd048bb82fbbf62e3f9de0f',
-    FB_APP_SECRET: 'ce6f59f0e98b1f037beaae855f56d3b6',
+    FB_PAGE_TOKEN: 'EAAE7FbDzhWQBALLKor4rw6Sygu6JdtObmJnoYuu7WHJsDYXsdZCcmXrJcTzjZAY4RmBh4koXeZC4YkJTA2LCLnweCCLcYOO5aBD9ALcn99UOAToRNmrGp01NbZBLgfjV2hwZBud7ZAKXrT2flwiIsx08k5GyymqzFKi3z8tWmigPnuZBA27bAAP',
+    FB_VERIFY_TOKEN: 'jandutbot',
+    API_AI_CLIENT_ACCESS_TOKEN: '630f930ab95e4b5d8e846945a7528614',
+    FB_APP_SECRET: '56a8faaa71e41470a94edee16fb2372e',
     SERVER_URL: "https://raghvendra-testbot.herokuapp.com/",
     SENDGRID_API_KEY:'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',
     EMAIL_FROM:"raghvendranaidu.naidu@gmail.com",
